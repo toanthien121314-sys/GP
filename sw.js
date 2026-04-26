4483982d1197140997d2c495eb5d2eb2fdbbce34
toanthien121314-sys <toanthien121314@gmail.com>
2026-04-26 08:58:18 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'bee-boy-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo219.png',
+  './logo512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
